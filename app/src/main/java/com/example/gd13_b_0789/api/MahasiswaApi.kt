8fe6d629694a56a24b5b3ea87621bed095219a0c
Jessicasgc <jessicaamadearahma@gmail.com>
2022-12-01 08:55:41 +0700

    GD Unit Testing

---
+++ app/src/main/java/com/example/gd13_b_0789/api/MahasiswaApi.kt
@@ -0,0 +1,14 @@
+package com.example.gd11_b_0789.api
+
+class MahasiswaApi {
+    companion object {
+        val BASE_URL = "http://10.53.11.195/ci4/public/"
+
+        val GET_ALL_URL = BASE_URL + "mahasiswa/"
+        val GET_BY_ID_URL = BASE_URL + "mahasiswa/"
+        val ADD_URL = BASE_URL + "mahasiswa"
+        val UPDATE_URL = BASE_URL + "mahasiswa/"
+        val DELETE_URL = BASE_URL + "mahasiswa/"
+
+    }
+}
---
+++ app/src/main/java/com/example/gd13_b_0789/models/Mahasiswa.kt
@@ -0,0 +1,7 @@
+package com.example.gd11_b_0789.models
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
